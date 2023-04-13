86d82d65521c4bb63e8f089748f54641bf58a5fa
Oliveiraflavia <flalima041015@gmail.com>
2023-04-13 08:16:43 -0300

    Criado laço while e for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int contador = 0;
+
+       // Laço para enquanto
+       while(contador < 10){
+           System.out.println("Contador = " + contador);
+           //Incremento = alterar o valor para o laço evoluir
+           contador = contador + 1;
+       }
+
+       // Laço para faça
+        for (int x=0; x < 10; x++){
+            System.out.println("Contador = " + x);
+        }
     }
 }
