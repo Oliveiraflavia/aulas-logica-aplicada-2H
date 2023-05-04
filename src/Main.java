a2dc47ad217c62d076c24a980f56f0e53ff4e21c
Oliveiraflavia <flalima041015@gmail.com>
2023-05-04 09:54:21 -0300

    Aula Introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       int contador = 0;
+        //Aula Introdutória de OO (Orientação á Objetos)
 
-       // Laço para enquanto
-       while(contador < 10){
-           System.out.println("Contador = " + contador);
-           //Incremento = alterar o valor para o laço evoluir
-           contador = contador + 1;
-       }
 
-       // Laço para faça
-        for (int x=0; x < 10; x++){
-            System.out.println("Contador = " + x);
-            // Laço para faça  enquanto
-            int cont = 1;
-            do {
-
-                // processo
-                System.out.println(cont);
-            }while(cont <= 10);
-        }
     }
 }
