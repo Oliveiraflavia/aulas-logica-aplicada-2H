32461f537db8486c7b95ed25cf7167604e9c36ac
Oliveiraflavia <flalima041015@gmail.com>
2023-05-18 11:05:00 -0300

    Entendi a mariana não

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         //Aula Introdutória de OO (Orientação á Objetos)
-
-
+        //Declaração de objeto
+        Pessoa adao;
+        //Instanciação do objeto
+        adao = new Pessoa();
+        //Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        //Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("Oi"));
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa {
+
+    // Atributos da Classe
+    public String nome;
+    public String sobrenome;
+
+    // métodos da Classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    }
+
+    public void criarPessoa(){}
+    return new Pessoa();
+}
