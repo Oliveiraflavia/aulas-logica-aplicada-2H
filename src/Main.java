8783037d04e8c21c1ac54827bb0a00a793d6f489
Flvia Oliveira <flalima041015@gmail.com>
2023-06-21 09:05:55 -0300

    Construtores, sobrecarga e encapsulamento;

--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,17 @@ public class Main {
         rainha.falar();
         rainha.comer();
 
+        // Criar produtos
+        Produto p1 = new Produto(1300.00);
+        Produto p2 = new Produto();
 
+        //p2.preco = 233.00;
+        p2.setPreco(233.00);
+        Produto p3;
+        p3 = new Produto("Placa Video" , 1300.00);
+        p3 = new Produto("Placa mãe" , 2500.00);
+
+        p2.setPreco(1.00);
 
     }
 }
